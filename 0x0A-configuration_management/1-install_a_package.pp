ee710999c4924efcf9fb5b82485501959fc4397c
Destiny Saturday <yiradesat@gmail.com>
2024-03-27 22:22:09 +0100

    fix: use pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
 # A puppet script to ensure flask is installed
 package { 'flask':
   ensure   => '2.1.0',
-  provider => '/usr/bin/pip3',
+  provider => 'pip3',
 }
 
 package { 'werkzeug':
   ensure   => '2.1.0',
-  provider => '/usr/bin/pip3',
+  provider => 'pip3',
 }
 
